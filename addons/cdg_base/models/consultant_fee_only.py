661d1a497fc6fd60d6a8a0f5e8c71d1a6097705e
Pikachu1980 <dark653855@gmail>
2017-12-20 11:51:16 +0800

    2017.12.20  at3208

---
+++ addons/cdg_base/models/consultant_fee_only.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+import logging, time
+
+class ConsultantFeeOnly(models.Model):
+    _name = 'consultant.fee'
+
+    consultant_id = fields.Char(string='舊顧問編號')
+    year = fields.Char(string='年度')
+    fee_id = fields.Char(string='收費編號')
+    fee_payable = fields.Integer(string='應繳金額')
+    fee_date = fields.Date(string='收費日期')
+    clerk_id = fields.Char(string='收費員編號')
+    worker_id = fields.Many2one(comodel_name='c.worker', string='輸入人員')
+    db_chang_date = fields.Date(string='異動日期')
+ #   fee_member = fields.Many2one(comodel_name='normal.p', string='關聯的捐款人')
+ #   member_data_ids = fields.Many2one(comodel_name='member.data', string='關聯的轉檔資料')
+
+
+    def data_input_form_DB(self):
+        data = self.env['base.external.dbsource'].search([])
+        lines = data.execute('SELECT * FROM 顧問收費檔')
+        i = 1
+        for line in lines:
+            _logger.error(' %s / %s', i, len(lines))
+            self.create({
+                'consultant_id': line[u'顧問編號'],
+                'year': line[u'年度'],
+                'db_chang_date': self.check_db_date(line[u'異動日期']),
+                'fee_payable': line[u'應繳金額'],
+                'fee_date': self.check_db_date(line[u'收費日期']),
+                'fee_id': line[u'收費編號'],
+                'clerk_id': line[u'收費員編號'],
+                'worker_id': self.check_user(line[u'輸入人員']),
+            })
+            i += 1
+
+    def check_db_date(self, date):
+        if date:
+            try:
+                time.strptime(date, "%Y-%m-%d")
+                return date
+            except:
+                return None
+        else:
+            return None
+
+    def check_user(self, row):
+        check = self.env['c.worker'].search([('w_id', '=', row)])
+        if check.id > 0:
+            return check.id
+        else:
+            return None
---
+++ addons/cdg_base/models/member_fee_only.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+import logging, time
+
+class MemberFeeOnly(models.Model):
+    _name = 'member_only.fee'
+
+    member_id = fields.Char(string='舊會員編號')
+    member_note_id = fields.Char(string='會員名冊編號')
+    year = fields.Char(string='年度')
+    fee_id = fields.Char(string='收費編號')
+    fee_payable = fields.Integer(string='應繳金額')
+    fee_date = fields.Date(string='收費日期')
+    clerk_id = fields.Char(string='收費員編號')
+    worker_id = fields.Many2one(comodel_name='c.worker', string='輸入人員')
+    db_chang_date = fields.Date(string='異動日期')
+#    fee_member = fields.Many2one(comodel_name='normal.p', string='關聯的捐款人')
+#    member_data_ids = fields.Many2one(comodel_name='member.data', string='關聯的轉檔資料')
+
+
+
+    def data_input_form_DB(self):
+        data = self.env['base.external.dbsource'].search([])
+        lines = data.execute('SELECT * FROM 會員收費檔')
+        i = 1
+        for line in lines:
+            _logger.error(' %s / %s', i, len(lines))
+            self.create({
+                'member_id': line[u'會員編號'],
+                'member_note_id': line[u'會員名冊編號'],
+                'year': line[u'年度'],
+                'db_chang_date': self.check_db_date(line[u'異動日期']),
+                'fee_payable': line[u'應繳金額'],
+                'fee_date': self.check_db_date(line[u'收費日期']),
+                'fee_id': line[u'收費編號'],
+                'clerk_id': line[u'收費員編號'],
+                'worker_id': self.check_user(line[u'輸入人員']),
+
+            })
+            i += 1
+
+    def check_db_date(self, date):
+        if date:
+            try:
+                time.strptime(date, "%Y-%m-%d")
+                return date
+            except:
+                return None
+        else:
+            return None
+
+    def check_user(self, row):
+        check = self.env['c.worker'].search([('w_id', '=', row)])
+        if check.id > 0:
+            return check.id
+        else:
+            return None
