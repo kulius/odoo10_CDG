4d2f79606e5d745eab83c06dd322363003635296
chci780117 <chci780117@gmail.com>
2017-12-21 10:04:17 +0800

    2017.12.21 at3210 收據分析表 修正

---
+++ addons/cdg_base/models/basic_setting.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import time
+from odoo import models, fields, api
+
+class CWorker(models.Model):
+    _name = 'basic.setting'
+
+    Basic_donations = fields.Integer(string="基本捐助款", default=100)
+    Annual_membership_fee = fields.Integer(string="會員年費", default=1200)
+    Annual_consultants_fee = fields.Integer(string="顧問年費", default=10000)
+    coffin_amount = fields.Integer(string="施棺滿足額", default=30000)
