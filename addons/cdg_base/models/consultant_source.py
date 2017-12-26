d1d480da00754880b333c722bfcb22657db69e28
Pikachu1980 <dark653855@gmail>
2017-12-26 15:58:44 +0800

    106.12.26 at3208 部分屬性更改以及畫面更動

--- addons/cdg_base/models/consultant_source.py
+++ addons/cdg_base/models/consultant_source.py
@@ -13,7 +13,7 @@ class ConsultantSource(models.Model):
 
     consultant_code = fields.Char("舊顧問編號")
     consultant_id = fields.Char('顧問編號')
-    type = fields.Char(string='人員種類')  # 人 種類
+    type = fields.Many2many(comodel_name='people.type', string='人員種類')
     name = fields.Char(string='姓名')
     birth = fields.Date(string='生日')
     cellphone = fields.Char(string='手機')
@@ -24,13 +24,13 @@ class ConsultantSource(models.Model):
     rec_addr = fields.Char(string='收據地址')
     zip2 = fields.Char() #聯絡地址的zip
     con_addr = fields.Char(string='聯絡地址')
-    hire_date = fields.Char(string='聘顧日期')
+    hire_date = fields.Date(string='聘顧日期')
     build_date = fields.Date(string='建檔日期')
     ps = fields.Text(string='備註')
-    cashier_code = fields.Char(string='收費員編號')  # M2o c.worker
+    cashier_code = fields.Many2one(comodel_name='c.worker', string='收費員')
     rec_send = fields.Boolean(string='收據寄送')
     report_send = fields.Boolean(string='報表寄送')
     thanks_send = fields.Boolean(string='感謝狀寄送')
-    sequence = fields.Integer(string='排序')
+    sequence = fields.Char(string='排序')
     key_in_user = fields.Many2one(comodel_name='c.worker', string='輸入人員', ondelete='cascade') # 原來的直接貼上
     db_chang_date = fields.Date(string='異動日期')
--- addons/cdg_base/models/member_source.py
+++ addons/cdg_base/models/member_source.py
@@ -14,7 +14,7 @@ class MemberSource(models.Model):
 
     member_code = fields.Char('舊會員編號')
     member_id = fields.Char('會員編號')
-    type = fields.Char(string='人員種類') #人 種類
+    type = fields.Many2many(comodel_name='people.type', string='人員種類')
     name = fields.Char(string='姓名')
     self_iden = fields.Char(string='身分證字號') #大多為空
     birth = fields.Date(string='生日')
@@ -28,9 +28,9 @@ class MemberSource(models.Model):
     con_addr = fields.Char(string='通訊地址')
     build_date = fields.Date(string='建檔日期')
     ps = fields.Text(string='備註')
-    cashier_code = fields.Char(string='收費員編號') # M2o c.worker
+    cashier_code = fields.Many2one(comodel_name='c.worker', string='收費員編號') # M2o c.worker
     rec_send = fields.Boolean(string='收據寄送')
     booklist = fields.Boolean(string='名冊列印')
-    sequence = fields.Integer(string='排序')
+    sequence = fields.Char(string='排序')
     key_in_user = fields.Many2one(comodel_name='c.worker', string='輸入人員', ondelete='cascade') # 原來的直接貼上
     db_chang_date = fields.Date(string='異動日期')
