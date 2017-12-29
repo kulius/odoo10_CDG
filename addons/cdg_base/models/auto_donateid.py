9a1da3cee786ad04e1b00835392b1364c4b79387
Pikachu1980 <dark653855@gmail>
2017-12-29 12:39:46 +0800

    106.12.29 at3208 自動編號初版

---
+++ addons/cdg_base/models/auto_donateid.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class AutoDonate(models.Model):
+    _name = "auto.donateid"
+
+    zip = fields.Char('郵政區號')
+    area_number = fields.Integer('區域編號')
