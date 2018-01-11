c2752d4995ac3910b120fea7a4b1f9e788497203
pikachu1980 <dark653855@gmail.com>
2018-01-11 11:20:53 +0800

    107.1.11 at3208 合併捐款者要同名同性

--- addons/cdg_base/wizard/chang_donater.py
+++ addons/cdg_base/wizard/chang_donater.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
-
+from odoo.exceptions import ValidationError
 
 class ChangDonater(models.Model):
     _name = 'chang.donater'
@@ -9,17 +9,20 @@ class ChangDonater(models.Model):
     from_target = fields.Many2one(comodel_name='normal.p', string='原始捐款人')
 
     def active_to_transfer(self):
-        # 更新眷屬
-        if self.from_target.donate_family1.ids:
-            for line in self.from_target.donate_family1:
-                line.parent = self.new_target.id
+        if(self.new_target.name != self.from_target.name):
+            raise ValidationError(u'目標捐款人和原始捐款人的姓名不相同')
 
-        # 更新歷史紀錄
-        if self.from_target.donate_history_ids.ids:
-            for line in self.from_target.donate_history_ids:
-                line.donate_member = self.new_target.id
+        else:
+            # 更新眷屬
+            if self.from_target.donate_family1.ids:
+                for line in self.from_target.donate_family1:
+                    line.parent = self.new_target.id
 
-        self.from_target.active = False
+            # 更新歷史紀錄
+            if self.from_target.donate_history_ids.ids:
+                for line in self.from_target.donate_history_ids:
+                    line.donate_member = self.new_target.id
 
+            self.from_target.active = False
 
 
