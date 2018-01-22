6bc46f840c3932d3e803ebe2b96ca388e8e519a7
chci780117 <chci780117@gmail.com>
2018-01-22 15:15:00 +0800

    2018.01.22 at3210 會員及顧問繳費狀況費查詢

---
+++ addons/cdg_base/wizard/consultant_fee_inquire.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+class consultantfeeinquire(models.Model):
+    _name = 'consultant.fee.inquire'
+
+    year = fields.Char(string='繳費年度')
+    payment = fields.Selection(selection=[(1, '已繳費'), (2, '未繳費')], string='繳費狀況')
+
+    def consultant_fee_search(self):
+        if self.year is False:
+            raise ValidationError(u'請輸入繳費年度!')
+        action = self.env.ref('cdg_base.consultant_fee_only_view_action').read()[0]
+        action['context'] ={} # remove default domain condition in search box
+        action['domain'] =[] # remove any value in search box
+        if self.payment is False:
+            action['domain'] = [('year', '=', self.year)]
+        if self.payment == 1:
+            action['domain'] = [('year', '=', self.year), ('normal_p_id.type.id', '=', 4), ('fee_date', '!=', False)]
+        elif self.payment == 2:
+            action['domain'] = [('year', '=', self.year), ('normal_p_id.type.id', '=', 4), ('fee_date', '=', False)]
+
+        action['views'] = [
+            [self.env.ref('cdg_base.consultant_fee_search_tree').id, 'tree'],
+        ]
+        return action
