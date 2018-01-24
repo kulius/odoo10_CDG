0946071f42d8bb64cd64e2df1a1dd09d5c15389f
pikachu1980 <dark653855@gmail.com>
2018-01-24 11:10:46 +0800

    Merge branch 'master' of D:\odoo\odoo10_CDG with conflicts.

--- addons/cdg_base/wizard/member_fee_inquire.py
+++ addons/cdg_base/wizard/member_fee_inquire.py
@@ -12,27 +12,37 @@ class memnberfeeinquire(models.Model):
     def memnber_fee_search(self):
         if self.year is False:
             raise ValidationError(u'請輸入繳費年度!')
+        number = 0
         action = self.env.ref('cdg_base.member_fee_only_view_action').read()[0]
         action['context'] ={} # remove default domain condition in search box
         action['domain'] =[] # remove any value in search box
         if self.payment is False and self.member_type is False:
             action['domain'] = [('year', '=', self.year),'|',('normal_p_id.type.id', '=', 2),('normal_p_id.type.id', '=', 3)]
+            number = len(self.env['associatemember.fee'].search([('year', '=', self.year),'|',('normal_p_id.type.id', '=', 2),('normal_p_id.type.id', '=', 3)]))
         elif self.member_type == 1:
             if self.payment ==1:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 2),('fee_date','!=',False)]
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 2),('fee_date','!=',False)]))
             elif self.payment ==2:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 2),('fee_date','=',False)]
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 2),('fee_date','=',False)]))
             elif self.payment is False:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 2)]
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 2)]))
         elif self.member_type == 2:
             if self.payment ==1:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 3),('fee_date','!=',False)]
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 3),('fee_date','!=',False)]))
             elif self.payment ==2:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 3),('fee_date','=',False)]
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 3),('fee_date','=',False)]))
             elif self.payment is False:
                 action['domain'] = [('year', '=', self.year),('normal_p_id.type.id', '=', 3)]
-
+                number = len(self.env['associatemember.fee'].search([('year', '=', self.year),('normal_p_id.type.id', '=', 3)]))
         action['views'] = [
             [self.env.ref('cdg_base.member_fee_search_tree').id, 'tree'],
         ]
+
+        action['limit'] = number
+
         return action
