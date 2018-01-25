11ba0096c571b9813800e51b132f225483abbb68
pikachu1980 <dark653855@gmail.com>
2018-01-25 10:21:48 +0800

    2018/1/25 at3208 會員繳費狀況可以利用當前的tree做新增，日期，年度代入預設

--- addons/cdg_base/models/consultant_fee_only.py
+++ addons/cdg_base/models/consultant_fee_only.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
+from datetime import datetime
 import logging, time
 
 class ConsultantFeeOnly(models.Model):
     _name = 'consultant.fee'
 
     consultant_id = fields.Char(string='舊顧問編號')
-    year = fields.Char(string='年度')
+    year = fields.Char(string='年度',default=datetime.today().year - 1911)
     fee_code = fields.Char(string='收費編號')
     fee_payable = fields.Integer(string='應繳金額')
-    fee_date = fields.Date(string='收費日期')
+    fee_date = fields.Date(string='收費日期',default=datetime.today())
     clerk_id = fields.Char(string='收費員編號')
     normal_p_id = fields.Many2one(comodel_name='normal.p', string='關聯的顧問')
     rec_addr = fields.Char(string='收據地址', related='normal_p_id.rec_addr')
--- addons/cdg_base/models/member_fee_only.py
+++ addons/cdg_base/models/member_fee_only.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 import logging, time
+from datetime import datetime
 
 class MemberFeeOnly(models.Model):
     _name = 'associatemember.fee'
 
     member_id = fields.Char(string='會員編號')
     member_note_code = fields.Char(string='會員名冊編號')
-    year = fields.Char(string='年度')
+    year = fields.Char(string='年度',default=datetime.today().year - 1911)
     fee_code = fields.Char(string='收費編號')
     fee_payable = fields.Integer(string='應繳金額')
-    fee_date = fields.Date(string='收費日期')
+    fee_date = fields.Date(string='收費日期',default=datetime.today())
     clerk_id = fields.Char(string='收費員編號')
     normal_p_id = fields.Many2one(comodel_name='normal.p', string='關聯的會員')
     rec_addr = fields.Char(string='收據地址', related='normal_p_id.rec_addr')
