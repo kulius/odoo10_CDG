e4dd03662955fea080d031bd209f599bd8512564
pikachu1980 <dark653855@gmail.com>
2018-01-25 12:11:55 +0800

    2018/1/25 at3208 顧問，會員匯出Excel檔資料異常 已修正

--- addons/cdg_base/report/member_list.py
+++ addons/cdg_base/report/member_list.py
@@ -11,40 +11,37 @@ class MemberListXlsx(ReportXlsx):
     def generate_xlsx_report(self,workbook,data,env):
         sheet = workbook.add_worksheet()
 
-        sheet.write(0, 1,u'年度')
-        sheet.write(0, 2, u'收費日期')
-        sheet.write(0, 3, u'收費編號')
-        sheet.write(0, 4, u'姓名')
-        sheet.write(0, 5, u'應繳金額')
-        sheet.write(0, 6, u'連絡電話')
-        sheet.write(0, 7, u'手機')
-        sheet.write(0, 8, u'收據地址')
-        sheet.write(0, 9, u'收費員編號')
+        sheet.write(0, 0,u'年度')
+        sheet.write(0, 1, u'收費日期')
+        sheet.write(0, 2, u'收費編號')
+        sheet.write(0, 3, u'姓名')
+        sheet.write(0, 4, u'應繳金額')
+        sheet.write(0, 5, u'連絡電話')
+        sheet.write(0, 6, u'手機')
+        sheet.write(0, 7, u'收據地址')
+        sheet.write(0, 8, u'收費員編號')
 
 
         for line in env:
             if not line:
                 continue
             else:
-                sheet.write(self.count,1,line.year)
-                sheet.write(self.count, 2, line.fee_date)
-                sheet.write(self.count, 3, line.fee_code)
-                sheet.write(self.count, 4, line.normal_p_id.name)
-                sheet.write(self.count, 5, line.fee_payable)
-                sheet.write(self.count, 6, line.con_phone)
-                sheet.write(self.count, 7, line.cellphone)
-                sheet.write(self.count, 8, line.rec_addr)
-                sheet.write(self.count, 9, line.clerk_id)
+                sheet.write(self.count, 0,line.year)
+                sheet.write(self.count, 1, line.fee_date)
+                sheet.write(self.count, 2, line.fee_code)
+                sheet.write(self.count, 3, line.normal_p_id.name)
+                sheet.write(self.count, 4, line.fee_payable)
+                sheet.write(self.count, 5, line.con_phone)
+                sheet.write(self.count, 6, line.cellphone)
+                sheet.write(self.count, 7, line.rec_addr)
+                sheet.write(self.count, 8, line.clerk_id)
                 self.count += 1
 
-        sheet.set_column(2, 3, 12)
-        sheet.set_column(6, 7, 12)
-        sheet.set_column(8, 8, 40)
-        sheet.set_column(9, 9, 12)
+        sheet.set_column(1, 2, 12)
+        sheet.set_column(5, 6, 12)
+        sheet.set_column(7, 7, 40)
+        sheet.set_column(8, 8, 12)
 
-        return {
-            'string': u'105 呵呵呵',
-        }
 
 
 
