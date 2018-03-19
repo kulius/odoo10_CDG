8f46891991b65de22acf1bfa3b00f737ac299d57
pikachu1980 <dark653855@gmail.com>
2018-03-19 16:45:36 +0800

    2018/3/19 at3208 更新會員名冊匯出功能

--- addons/cdg_base/report/member_list.py
+++ addons/cdg_base/report/member_list.py
@@ -6,45 +6,34 @@ from odoo.addons.report_xlsx.report.report_xlsx import ReportXlsx
 
 class MemberListXlsx(ReportXlsx):
 
+
     def generate_xlsx_report(self,workbook,data,env):
+
+        array = self.env['associatemember.fee']
+        # data['data_id']
+
         sheet = workbook.add_worksheet()
         count = 1
 
-        sheet.write(0, 0,u'年度')
-        sheet.write(0, 1, u'收費日期')
-        sheet.write(0, 2, u'收費情況')
-        sheet.write(0, 3, u'收費編號')
-        sheet.write(0, 4, u'姓名')
-        sheet.write(0, 5, u'應繳金額')
-        sheet.write(0, 6, u'連絡電話')
-        sheet.write(0, 7, u'手機')
-        sheet.write(0, 8, u'收據地址')
-        sheet.write(0, 9, u'收費員')
+        sheet.write(0, 0,u'會員編號')
+        sheet.write(0, 1, u'姓名')
+        sheet.write(0, 2, u'收據地址')
 
-        for line in env:
-            if not line:
+        for line in data['docs']:
+            data = array.browse(line)
+            if not data:
                 continue
             else:
-                if not line.fee_date:
-                    sheet.write(count, 2, u"未繳費")
-                else:
-                    sheet.write(count, 2, u"已繳費")
-                sheet.write(count,0,line.year)
-                sheet.write(count, 1, line.fee_date)
-                sheet.write(count, 3, line.fee_code)
-                sheet.write(count, 4, line.normal_p_id.name)
-                sheet.write(count, 5, line.fee_payable)
-                sheet.write(count, 6, line.con_phone)
-                sheet.write(count, 7, line.cellphone)
-                sheet.write(count, 8, line.rec_addr)
-                sheet.write(count, 9, line.normal_p_id.cashier_name.name)
+                sheet.write(count, 0, data.member_code)
+                sheet.write(count, 1, data.member_name)
+                sheet.write(count, 2, data.fee_code)
                 count += 1
 
-        sheet.set_column(1, 1, 12)
-        sheet.set_column(3, 3, 12)
-        sheet.set_column(6, 7, 12)
-        sheet.set_column(8, 8, 40)
-        sheet.set_column(9, 9, 12)
+        # sheet.set_column(1, 1, 12)
+        # sheet.set_column(3, 3, 12)
+        # sheet.set_column(6, 7, 12)
+        # sheet.set_column(8, 8, 40)
+        # sheet.set_column(9, 9, 12)
 
         count = 1
 
--- addons/cdg_base/wizard/member_fee_inquire.py
+++ addons/cdg_base/wizard/member_fee_inquire.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import collections
 
 class memnberfeeinquire(models.Model):
     _name = 'memnber.fee.inquire'
@@ -10,6 +11,7 @@ class memnberfeeinquire(models.Model):
     end_year = fields.Char(string='繳費年度-訖')
     member_type = fields.Selection(selection=[(1, '基本會員'), (2, '贊助會員')], string='會員種類')
     payment = fields.Selection(selection=[(1, '已繳費'), (2, '未繳費')], string='繳費狀況')
+
 
     def memnber_fee_search(self):
         star_time = int(self.star_year)
@@ -50,3 +52,89 @@ class memnberfeeinquire(models.Model):
         ]
         action['limit'] = number
         return action
+
+    def print_excel(self):
+        star_time = int(self.star_year)
+        end_time = int(self.end_year)
+        data_id = []
+        temp_list = []
+        number = end_time - star_time + 1
+
+        if self.star_year is False or self.end_year is False:
+            raise ValidationError(u'請正確輸入繳費年度!')
+        if star_time > end_time:
+            raise ValidationError(u'[繳費年度-起] 不得大於 [繳費年度-訖]')
+        if self.payment is False or self.member_type is False:
+           raise ValidationError(u'請設定會員種類以及繳費狀況')
+
+        if self.member_type == 1:
+            if self.payment == 1:
+                data = self.env['associatemember.fee'].search([('year', '>=', star_time),('year', '<=', end_time),('normal_p_id.type.id', '=', 2),('fee_date','!=',False),('normal_p_id.booklist','=',True)],order='normal_p_id asc')
+                s = collections.Counter()
+                for line in data:
+                    s[line.normal_p_id.id] += 1
+                    if s[line.normal_p_id.id] == number:
+                        data_id.append(line.id)
+                docargs = {
+                    'docs': data_id,
+                }
+                s.clear()
+                return {
+                    'type': 'ir.actions.report.xml',
+                    'report_name': 'cdg_base.member_list.xlsx',
+                    'datas': docargs
+                }
+            elif self.payment == 2:
+                data = self.env['associatemember.fee'].search([('year', '>=', star_time), ('year', '<=', end_time), ('normal_p_id.type.id', '=', 2), ('fee_date', '=', False),('normal_p_id.booklist','=',True)], order='normal_p_id asc')
+                for line in data:
+                    if line.normal_p_id.id in temp_list:
+                        continue
+                    else:
+                       data_id.append(line.id)
+                    temp_list.append(line.normal_p_id.id)
+                docargs = {
+                    'docs': data_id,
+                }
+                return {
+                    'type': 'ir.actions.report.xml',
+                    'report_name': 'cdg_base.member_list.xlsx',
+                    'datas': docargs
+                }
+        elif self.member_type == 2:
+            if self.payment == 1:
+                data = self.env['associatemember.fee'].search([('year', '>=', star_time), ('year', '<=', end_time), ('normal_p_id.type.id', '=', 3),('fee_date', '!=', False),('normal_p_id.booklist','=',True)], order='normal_p_id asc')
+                s = collections.Counter()
+                for line in data:
+                    s[line.normal_p_id.id] += 1
+                    if s[line.normal_p_id.id] == number:
+                        data_id.append(line.id)
+                docargs = {
+                    'docs': data_id,
+                }
+                s.clear()
+                return {
+                    'type': 'ir.actions.report.xml',
+                    'report_name': 'cdg_base.member_list.xlsx',
+                    'datas': docargs
+                }
+            elif self.payment == 2:
+                data = self.env['associatemember.fee'].search([('year', '>=', star_time), ('year', '<=', end_time), ('normal_p_id.type.id', '=', 3),('fee_date', '=', False),('normal_p_id.booklist','=',True)], order='normal_p_id asc')
+                for line in data:
+                    if line.normal_p_id.id in temp_list:
+                        continue
+                    else:
+                       data_id.append(line.id)
+                    temp_list.append(line.normal_p_id.id)
+                docargs = {
+                    'docs': data_id,
+                }
+                return {
+                    'type': 'ir.actions.report.xml',
+                    'report_name': 'cdg_base.member_list.xlsx',
+                    'datas': docargs
+                }
+
+
+
+
+        # return True
