107f8b27dc47a9543aa3e3d18a2c99416254fa3e
chci780117 <chci780117@gmail.com>
2018-03-21 11:35:51 +0800

    2018.03.21 at3210 施棺管理單筆捐助功能修復

--- addons/cdg_base/models/coffin_donation.py
+++ addons/cdg_base/models/coffin_donation.py
@@ -5,13 +5,23 @@ class CoffinDonation(models.Model):
     _name = 'coffin.donation'
 
     name = fields.Char(string='捐款者姓名', related='donate_order_id.donate_member.name')
-    donate_price = fields.Integer(string='施棺捐款金額', related='donate_order_id.donate')
+    donate = fields.Integer(string='施棺捐款金額', related='donate_order_id.donate')
+    donate_price = fields.Integer(string='施棺捐款金額(已用)', related='donate_order_id.used_money')
+    use_amount = fields.Boolean(string='施棺捐款是否已支用', related='donate_order_id.use_amount')
+    available_balance = fields.Integer(string='可用餘額', related='donate_order_id.available_balance')
 
     coffin_donation_id = fields.Many2one(comodel_name='coffin.base')
     old_coffin_donation_id = fields.Many2one(comodel_name='coffin.base')
     donate_single_id = fields.Many2one(comodel_name='donate.single', string='捐款編號')
     donate_order_id = fields.Many2one(comodel_name='donate.order', string='捐款者 (可用餘額)' , domain=[('donate_type', '=', 3),('available_balance', '!=', 0)])
     donate_id =  fields.Char(string='捐款編號', related='donate_order_id.donate_id')
+
+    @api.onchange('donate_order_id')
+    def set_used_money(self):
+        self.donate_price = self.donate
+        self.donate = 0
+        self.available_balance = 0
+        self.use_amount = True
 
     def data_input_from_database(self):
         data = self.env['base.external.dbsource'].search([])
