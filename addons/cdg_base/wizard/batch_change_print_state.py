bcdd75abb1304887f14845ac7f0e5673bc550b29
pikachu1980 <dark653855@gmail.com>
2018-04-10 14:29:26 +0800

    2018/04/10 at3208 批次變更調整

--- addons/cdg_base/wizard/batch_change_print_state.py
+++ addons/cdg_base/wizard/batch_change_print_state.py
@@ -9,9 +9,10 @@ class changestate(models.Model):
     end_donate_id = fields.Char(string='收據編號-訖')
     change_user = fields.Many2one(comodel_name='res.users', string='變更人員', default=lambda self: self.env.uid)
     key_in_user = fields.Many2one(comodel_name='res.users', string='建檔人員')
+    cashier_code = fields.Many2one(comodel_name='cashier.base', string='收費員')
 
     def change_state(self):
-        datas = self.env['donate.single'].search([('donate_id','>=',self.star_donate_id),('donate_id','<=',self.end_donate_id),('key_in_user','=',self.key_in_user.id)])
+        datas = self.env['donate.single'].search([('donate_id','>=',self.star_donate_id),('donate_id','<=',self.end_donate_id),('key_in_user','=',self.key_in_user.id),('work_id','=',self.cashier_code.id)])
         for line in datas:
             if line.state == 2:
                 line.state = 1
--- addons/cdg_base/wizard/wizard_donate_time.py
+++ addons/cdg_base/wizard/wizard_donate_time.py
@@ -6,14 +6,14 @@ from odoo.exceptions import ValidationError
 class WizardCashierDonor(models.Model):
     _name = 'wizard.donate.time'
     cashier_code = fields.Many2one(comodel_name='cashier.base',string='收費員')
-    donate_start_date = fields.Date('捐款日期-起')
-    donate_end_date = fields.Date('捐款日期-訖')
+    donate_id_start = fields.Char('收據編號-起')
+    donate_id_end = fields.Char('收據編號-訖')
     key_in_user_code = fields.Many2one(comodel_name='res.users',string="建檔人員")
     donate_date = fields.Date('捐款日期')
 
     def donate_time(self):
 
-        data = self.env['donate.single'].search([('key_in_user','=',self.key_in_user_code.id),('donate_date','>=',self.donate_start_date),('donate_date','<=',self.donate_end_date),('work_id','=',self.cashier_code.id),('state','=',1)])
+        data = self.env['donate.single'].search([('key_in_user','=',self.key_in_user_code.id),('donate_id','>=',self.donate_id_start),('donate_id','<=',self.donate_id_end),('work_id','=',self.cashier_code.id),('state','=',1)])
 
         for line in data:
             line.donate_date = self.donate_date
