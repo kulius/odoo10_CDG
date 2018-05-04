3091f720088e9d78fd160dd2341f8e3244e79ea0
chci780117 <chci780117@gmail.com>
2018-05-04 08:30:11 +0800

    2018.05.01 at3120 1.捐款作業的作廢按鈕BUG修正

--- addons/cdg_base/wizard/wizard_abandon_single.py
+++ addons/cdg_base/wizard/wizard_abandon_single.py
@@ -9,27 +9,15 @@ class WizardCashierDonor(models.Model):
     donate_single_code = fields.Many2one(comodel_name='donate.single', string='捐款檔')
 
     def set_abandon(self):
-
-        data = self.env['donate.single'].search([('donate_id','=',self.donate_single_code.donate_id)])
+        data = self.env['donate.single'].search([('id','=',self.donate_single_code.id)])
 
         for line in data:
             if line.state == 3:
                 raise ValidationError(u'本捐款單已作廢!!')
-
-            for line2 in line.donate_list:
-                if line2.used_money != 0:
-                    raise ValidationError(
-                        u'本捐款單的 %s 先生/小姐 施棺捐款 %s 元整，已支出!! 因此無法作廢或退費，感謝您的善心' % (line2.donate_member.name, line2.donate))
-
-            for line2 in line.donate_list:
-                line2.state = 2
-                line2.active = False
-
-
+            for row in line.donate_list:
+                if row.used_money != 0:
+                    raise ValidationError(u'本捐款單的 %s 先生/小姐 施棺捐款 %s 元整，已支出!! 因此無法作廢或退費，感謝您的善心' % (row.donate_member.name, row.donate))
+                row.state = 2
+                row.active = False
             line.state = 3
             line.active = False
-
-
-
-
-
