e5579072a9507037029905778560ab3c78f19d3e
pikachu1980 <dark653855@gmail.com>
2018-06-20 09:41:43 +0800

    2018/06/19 at3208  收費員批次捐款 捐款總額修正

--- addons/cdg_base/models/credit_statistics.py
+++ addons/cdg_base/models/credit_statistics.py
@@ -11,6 +11,11 @@ class creditstatistics(models.TransientModel):
     season_debit = fields.Integer(string='季扣款人數', readonly=True)
     year_debit = fields.Integer(string='年扣款人數', readonly=True)
     once_debit = fields.Integer(string='單次扣款人數', readonly=True)
+    five_debit_money = fields.Integer(string='五日扣款金額', readonly=True)
+    twenty_debit_money = fields.Integer(string='二十日扣款金額', readonly=True)
+    season_debit_money = fields.Integer(string='季扣款金額', readonly=True)
+    year_debit_money = fields.Integer(string='年扣款金額', readonly=True)
+    once_debit_money = fields.Integer(string='單次扣款金額', readonly=True)
 
     def count_people(self):
         self.credit_statistics = len(self.env['normal.p'].search([('debit_method','!=',False),('is_donate','=',True)]))
@@ -19,3 +24,23 @@ class creditstatistics(models.TransientModel):
         self.season_debit = len(self.env['normal.p'].search([('debit_method', '=', 3),('is_donate','=',True)]))
         self.year_debit = len(self.env['normal.p'].search([('debit_method', '=', 4),('is_donate','=',True)]))
         self.once_debit = len(self.env['normal.p'].search([('debit_method', '=', 5),('is_donate','=',True)]))
+
+        data1 = self.env['normal.p'].search([('debit_method','=',1),('is_donate','=',True)])
+        for data in data1:
+            self.five_debit_money += data.credit_total_money
+
+        data2 = self.env['normal.p'].search([('debit_method', '=', 2),('is_donate','=',True)])
+        for data in data2:
+            self.twenty_debit_money += data.credit_total_money
+
+        data3 = self.env['normal.p'].search([('debit_method', '=', 3),('is_donate','=',True)])
+        for data in data3:
+            self.season_debit_money += data.credit_total_money
+
+        data4 = self.env['normal.p'].search([('debit_method', '=', 4),('is_donate','=',True)])
+        for data in data4:
+            self.year_debit_money += data.credit_total_money
+
+        data5 = self.env['normal.p'].search([('debit_method', '=', 5), ('is_donate', '=', True)])
+        for data in data5:
+            self.once_debit_money += data.credit_total_money
